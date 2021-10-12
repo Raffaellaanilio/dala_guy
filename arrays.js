d8e5c31d4312144dfb11f8061372f16b2565a55c
Raffaellaanilio <57917681+Raffaellaanilio@users.noreply.github.com>
2021-10-11 22:00:13 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,57 @@
+var estructura = [
+ [1, 'Damage and Loss Assessment (DaLA)', 'Evaluación de Daños y Pérdidas (DaLA)'],
+ [2, 'Guyana afectada por inundaciones adversas, Junio de 2021', 'Guyana impacted by adverse flooding, June 2021'],
+ [3, '<img src="./logo-cepal.png">', '<img src="./gfdrr_logo.png">'],
+ [4, ''],
+ [5, ], //idioma?
+]
+
+var listado_temas = [
+[1, 'Eventos','Events'],
+[2, 'Población afectada','Affected population'],
+[3, 'Educación', 'Education'],
+[4, 'Salud','Health'],
+[5, 'Vivienda','Housing'],
+[6, 'Agua y saneamiento','Water and sanitation'],
+[7, 'Electricidad y energía','Power and energy'],
+[8, 'Telecomunicaciones','Telecommunications'],
+[9, 'Transporte y vialidad', 'Transportation'],
+[10, 'Agricultura', 'Agriculture'],
+[11, 'Turismo y comercio', 'Tourism'],
+[12, 'Medio ambiente', 'Environment'],
+[13, 'Impacto macroeconómico', 'Macroeconomic impact'],
+[14, 'Evaluación DaLa', 'DaLa evaluation'],
+]
+
+var mensajes_claves = [
+[1, 'mensaje clave escuelas'],
+]
+
+var capas_geonode = [
+    [1, 3, 'geonode:GUY_SCHOOLS_REG3678910', 'Escuelas', 'Schools', '', 1],
+    [2, 3, 'geonode:GUY_SCHOOLS_REG1245', '', '', ''],
+    [3, 6, 'geonode:GUY_RIVERS', 'Ríos', 'Rivers', '', ''],
+    [4, 1, 'geonode:GUY_REGIONS', 'Regiones', 'Regions', ''],
+    [5, 12, 'geonode:GUY_NATURAL_ZONES', 'Zonas naturales', 'Natural zones', ''],
+    [6, 5, 'geonode:GUY_MAIN_TOWNS_POLY', 'Principales ciudades (polígonos)', 'Main towns (polygons)', ''],
+    [7, 5, 'geonode:GUY_MAIN_TOWNS_POINTS', 'Principales ciudades (puntos)', 'Main towns (points)', ''],
+    [8, 9, 'geonode:GUY_MAIN_ROADS', 'Caminos principales', 'Main roads', ''],
+    [9, 6, 'geonode:GUY_MAIN_RIVERS', 'Ríos principales', 'Main rivers', ''],
+    [10, 12, 'geonode:GUY_LAND_USE_COASTAL_PLAIN_2010', 'Uso de suelo costero', 'Coastal land use', '2010', ''],
+    [11, 12, 'geonode:GUY_LAND_COVER_USE_2010', 'Uso de cobertura de suelo', 'Land cover use', '2010'],
+    [12, 5, 'geonode:GUY_HOUSES_OSM', 'Viviendas', 'Housing', 'Open Street Map', ''],
+    [13, 1, 'geonode:GUY_FLOODED_AREA_JUN_MAY_2021', 'Área inundada junio-mayo 2021', '', ''],
+    [14, 9, 'geonode:GUY_AIRPORTS', 'Aeropuertos', 'Airports', ''],
+    [15, 1, 'geonode:Flood_area_09_06_2021_UpperMazaruni_RCM2_MS', 'Área inundada "Upper Mazaruni" (09/06/2021)', 'Flooded area "Upper Mazaruni" (09/06/2021)'],
+    [16, 1, 'geonode:Flood_area_23_06_2021_Upper_Mazaruni_ST1_MS', 'Área inundada "Upper Mazaruni" (23/06/2021)', 'Flooded area "Upper Mazaruni" (23/06/2021)', ''],
+    [17, 1, 'geonode:Flood_area_19_05_2021_Rupununi_ST1_MS', 'Área inundada "Rupununi" (19/05/2021)', 'Flooded area "Rupununi" (19/05/2021)', ''],
+    [18, 1, 'geonode:Flood_area_20_05_2021_Rupununi_ST2_MS', 'Área inundada "Rupununi" (20/05/2021)', 'Flooded area "Rupununi" (20/05/2021)', ''],
+    [19, 1, 'geonode:Flood_area_12_06_2021_Region9_ST1_MS', 'Área inundada "region 9" (12/06/2021)', 'Flooded area "region 9" (12/06/2021)', ''],
+    [20, 1, 'geonode:Flood_area_07_06_2021_Kwakwani_RCM2_MS', 'Área inundada "Kwakwani" (07/06/2021)', 'Flooded area "Kwakwani" (07/06/2021)', ''],
+    [21, 1, 'geonode:Flood_area_06_06_2021_Guayna_VIIRS_MS', 'Área inundada "Guayna" (06/06/2021)', 'Flooded area "Guayna" (06/06/2021)', ''],
+    [22, 1, 'geonode:Flood_area_08_06_2021_Guayna_VIIRS_ABI_MS', 'Área inundada "Guayna" (08/06/2021)', 'Flooded area "Guayna" (08/06/2021)', ''],
+    [23, 1, 'geonode:Flood_area_07_06_2021_COP_MS', 'Área inundada (07/06/2021)', 'Flooded area'],    
+]
+
+
+
